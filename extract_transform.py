871c5f6186107e2720e8ae027b43b38646fbaeac
lallinger-arbeit <patrick.lallinger@rohde-schwarz.com>
2019-09-10 15:44:30 +0200

    updated

--- extract_transform.py
+++ extract_transform.py
@@ -11,20 +11,14 @@ def extract_transform(data):
     user=os.environ['STORAGE_USERNAME']
     password=os.environ['STORAGE_PASSWORD']
     container_name ='landingzone'
+    
     block_blob_service = BlockBlobService(account_name=user, account_key=password)
 
-    local_path=os.path.expanduser("~/tmp")
-    local_file_name =data["name"]+"_" + str(uuid.uuid4()) + ".csv"
-    full_path_to_file =os.path.join(local_path, local_file_name)
-
+    blobname =data["name"]+"_" + str(uuid.uuid4()) + ".csv"
     csv=data["name"]+","+str(data["count"])+","+data["meta"]
-    f = open(full_path_to_file,  'w')
-    f.write(csv)
-    f.close()
+    block_blob_service.create_blob_from_text(container_name, blobname, csv)
 
-    block_blob_service.create_blob_from_path(container_name, local_file_name, full_path_to_file)
-    os.remove(full_path_to_file)
-    return "Uploaded blob "+local_file_name+" to landing zone\n"
+    return "Uploaded blob "+blobname+" to landing zone\n"
 
 
 @app.route('/', methods=['PUT','POST'])
